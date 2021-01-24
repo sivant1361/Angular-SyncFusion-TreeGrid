df51b438b088677558b2a41773b0c1d0e58e2bfb
sivant1361 <55569255+sivant1361@users.noreply.github.com>
2021-01-24 23:15:40 +0530

    angular syncfusion tree grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { sampleData } from './datasource';
+import {
+  PageSettingsModel,
+  SortSettingsModel,
+} from '@syncfusion/ej2-angular-treegrid';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'syncfusion-grid';
+export class AppComponent implements OnInit {
+  public data: Object[];
+  public pageSettings: PageSettingsModel;
+  public sortSettings: SortSettingsModel;
+  title = 'Sync Fusion Grids';
+
+  ngOnInit(): void {
+    this.data = sampleData;
+    this.pageSettings = { pageSize: 15 };
+    this.sortSettings = {
+      columns:[
+        {field:'taskName', direction:'Ascending'},
+        {field:'taskID', direction:'Descending'},
+      ]
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {
+  TreeGridModule,
+  PageService,
+  SortService,
+  FilterService,
+} from '@syncfusion/ej2-angular-treegrid';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, TreeGridModule],
+  providers: [PageService, SortService, FilterService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
